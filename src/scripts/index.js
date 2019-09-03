e8842950db1b02468be13269a42f8898efeb8f59
Philipp Bütler <philipp.buetler@trivelop.ch>
2019-09-03 10:15:27 +0200

    Tech Radar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,3 +9,25 @@ let techRadar = {
 	  "Languages & Frameworks": ["TypeScript", "Rust"]
   }
 };
+
+var div = document.createElement("div");
+div.innerHTML = techRadar.Meta.generatedAt;
+document.body.appendChild(div);
+
+let headUl = document.createElement('ul');
+
+for (var group in techRadar.TechRadar) {
+    let li = document.createElement('li');
+    li.innerHTML = group;
+    let innerUL = document.createElement('ul');
+    
+    for (var text in techRadar.TechRadar[group]) {
+        let innerLI = document.createElement('li');
+        innerLI.innerHTML = techRadar.TechRadar[group][text];
+        innerUL.appendChild(innerLI);
+    }
+    
+    li.appendChild(innerUL);
+    headUl.appendChild(li);
+}
+document.body.appendChild(headUl);
